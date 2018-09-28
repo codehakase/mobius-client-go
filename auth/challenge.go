d2ee47337a370215d4eb2d90c3b11e6713c826fb
Francis Sunday <codehakase@gmail.com>
2018-09-28 19:17:00 +0100

    add auth/challenge
    add methods to generate challenge transactions

---
+++ auth/challenge.go
@@ -0,0 +1,69 @@
+package auth
+
+import (
+	"crypto/sha256"
+	"log"
+	"math"
+	"math/rand"
+	"time"
+
+	mc "github.com/codehakase/mobius-client-go/client"
+	"github.com/stellar/go/build"
+	"github.com/stellar/go/keypair"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// Challenge represents a model which generates challenge transactions on
+// developer's account
+type Challenge struct{}
+
+// Call generates a challenge transaction signed by the developer's private key
+func (c *Challenge) Call(devSecret string, expersIn int) string {
+	keypairHash := sha256.Sum256([]byte(devSecret))
+	kp, err := keypair.FromRawSeed(keypairHash)
+	if err != nil {
+		log.Fatalf("failed to create keypair from developer secret key, err: %v", err)
+	}
+	randomKp, _ := keypair.Random()
+	tx, err := build.Transaction(
+		build.Payment(
+			build.SourceAccount{AddressOrSeed: randomKp.Address()},
+			build.Destination{AddressOrSeed: kp.Address()},
+			build.Sequence{Sequence: c.randomSequence()},
+			build.NativeAmount{Amount: "0.000001"},
+		),
+	)
+	if err != nil {
+		log.Fatalf("failed to build challenge transaction, err: %v", err)
+	}
+	txe, err := tx.Sign(kp.Address())
+	if err != nil {
+		log.Fatalf("failed to sign challenge transaction, err: %v", err)
+	}
+	txtEnvelopeStr, err := txe.Base64()
+	if err != nil {
+		log.Fatalf("failed to generate txt envelope str for challenge txt, err: %v", err)
+	}
+	return txtEnvelopeStr
+}
+
+func (c *Challenge) randomSequence() uint64 {
+	return uint64(99999999 - math.Floor(rand.Float64()*65536))
+}
+
+func (c *Challenge) memo() build.MemoText {
+	return build.MemoText{Value: "Mobius authentication"}
+}
+
+func (c *Challenge) buildTimeBounds(exp int64) build.Timebounds {
+	if exp < 1 {
+		exp = mc.ChallengeExpiresIn
+	}
+	return build.Timebounds{
+		MinTime: uint64(math.Floor(float64(time.Now().UnixNano() / 1000))),
+		MaxTime: uint64(math.Floor(float64(time.Now().UnixNano()/1000 + exp))),
+	}
+}
