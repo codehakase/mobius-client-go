4d0d86f9dbaf4cf820c94d0a801636f5f0fa2035
Francis Sunday <codehakase@gmail.com>
2018-10-05 15:18:36 +0100

    use validateTx from utils

--- auth/sign.go
+++ auth/sign.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/codehakase/mobius-client-go/utils"
 	"github.com/stellar/go/build"
-	"github.com/stellar/go/keypair"
 	"github.com/stellar/go/xdr"
 )
 
@@ -25,7 +24,7 @@ func (s *Sign) Call(userSecret, xdrs, address string) string {
 		log.Fatalf("failed to parse dev key pair, err: %v", err)
 	}
 	txt := &build.TransactionBuilder{TX: &tx.Tx}
-	_ = s.validate(devKeypair, tx, txt)
+	_ = utils.ValidateTx(devKeypair, tx, txt)
 	txe, err := txt.Sign(kp.Seed())
 	if err != nil {
 		log.Fatalf("failed to sign challenge transaction, err: %v", err)
@@ -35,21 +34,4 @@ func (s *Sign) Call(userSecret, xdrs, address string) string {
 		log.Fatalf("failed to generate txt envelope str, err: %v", err)
 	}
 	return txtEnvelopeStr
-}
-
-func (s *Sign) validate(devKeypair *keypair.Full, tx *xdr.TransactionEnvelope, t *build.TransactionBuilder) bool {
-	if tx.Signatures == nil || len(tx.Signatures) < 1 {
-		return false
-	}
-	hash, err := t.Hash()
-	if err != nil {
-		log.Fatalf("failed to retrieve transaction hash, err: %v", err)
-	}
-	for _, signature := range tx.Signatures {
-		if err := devKeypair.Verify(hash[:], signature.Signature); err != nil {
-			return true
-		}
-	}
-	log.Fatalf("wrong challenge transaction signature")
-	return false
 }
