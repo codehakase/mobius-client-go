c8d9bd6bf1f255da8004b23825f7e0f13b33df31
Francis Sunday <codehakase@gmail.com>
2018-10-05 15:17:25 +0100

    move validateTx method to utils package

--- utils/utils.go
+++ utils/utils.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/stellar/go/build"
 	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/xdr"
 )
 
 func KPFromSeed(seed string) *keypair.Full {
@@ -29,4 +31,21 @@ func KPFromAddress(address string) *keypair.Full {
 		os.Exit(1)
 	}
 	return kp.(*keypair.Full)
+}
+
+func ValidateTx(devKeypair *keypair.Full, tx *xdr.TransactionEnvelope, t *build.TransactionBuilder) bool {
+	if tx.Signatures == nil || len(tx.Signatures) < 1 {
+		return false
+	}
+	hash, err := t.Hash()
+	if err != nil {
+		log.Fatalf("failed to retrieve transaction hash, err: %v", err)
+	}
+	for _, signature := range tx.Signatures {
+		if err := devKeypair.Verify(hash[:], signature.Signature); err != nil {
+			return true
+		}
+	}
+	log.Fatalf("wrong challenge transaction signature")
+	return false
 }
